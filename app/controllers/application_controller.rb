6ec6cd5c04a9be3cddb3ef5d7aa178c06af965b9
Maria Reedstrom <maria.reedstrom@gmail.com>
2022-05-10 10:37:45 -0500

    commit tree cd99003386d9842458c9e15d275b41c9cec48f99

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+
+
 
   get '/games' do
     games = Game.all.order(:title).limit(10)
@@ -15,5 +19,31 @@ class ApplicationController < Sinatra::Base
       } }
     })
   end
+
+  delete '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
+
 
 end
